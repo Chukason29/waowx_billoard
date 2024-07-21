823d10acb55b5551d594204b5d6a871e70f68215
Chukason29 <Chukwuebukaalaegbu@gmail.com>
2024-07-21 16:39:07 +0100

    added mobile menu

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const menuIcon = document.getElementById("menu-icon");
+const menuNav = document.getElementById("menu-nav");
+const closeMenu = document.getElementById("close-menu");
+const openMenu = document.getElementById("open-menu");
+
+function toggleMenu() {
+    if (!menuNav.classList.contains("show-menu")) {
+        menuNav.classList.add("show-menu");
+        menuNav.classList.remove("hide-menu");
+    } else {
+        menuNav.classList.add("hide-menu");
+        menuNav.classList.remove("show-menu");
+    }
+}
+
+//toggling through the menu system
+openMenu.addEventListener("click", toggleMenu);
+closeMenu.addEventListener("click", toggleMenu);
+
+/*$(document).ready(function() {
+    $(window).scroll(function() {
+        $('.animated-element').each(function() {
+            var position = $(this).offset().top + 75;
+            var scroll = $(window).scrollTop();
+
+            if (position < scroll + $(window).height()) {
+				//$(this).addClass('animate__animated');
+				$(this).addClass('visible');
+            }
+        });
+        $('.header')
+    });
+});*/
